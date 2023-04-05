c6b7ec18f5e48f9b9d3fdb51dc61fd1027004611
HP <adiibrahimov@gmail.com>
2023-04-05 21:12:43 +0600

    ooo

--- src/dao/daoClasses/PatientDaoImpl.java
+++ src/dao/daoClasses/PatientDaoImpl.java
@@ -5,8 +5,10 @@ import db.DateBase;
 import model.Hospital;
 import model.Patient;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class PatientDaoImpl implements PatientDao {
     DateBase dateBase = new DateBase();
@@ -31,32 +33,76 @@ public class PatientDaoImpl implements PatientDao {
 
     @Override
     public String addPatientsToHospital(Long id, List<Patient> patients) {
-        System.out.println("Hello");
-        return null;
+        boolean loop = false;
+        for (Hospital h : dateBase.getHospitals()) {
+            if (h.getId() == id) {
+                h.setPatients(patients);
+                loop = false;
+            } else {
+                loop = true;
+            }
+        }
+        if (loop) {
+            return "Tabylgan jok !";
+        }
+        return "Added";
     }
 
     @Override
     public String updatePatientById(Long id, Patient patientsNewInfo) {
-        return null;
+        for (Patient p : dateBase.getPatients()) {
+            if (id == p.getId()) {
+                p.setFirstName(patientsNewInfo.getFirstName());
+                p.setLastName(patientsNewInfo.getLastName());
+                p.setAge(patientsNewInfo.getAge());
+                p.setGender(patientsNewInfo.getGender());
+            }
+        }
+        return "updated..";
     }
 
     @Override
     public void removePatientById(Long id) {
+        for (Patient p : dateBase.getPatients()) {
+            if (p.getId() == id) {
+                dateBase.getPatients().remove(p);
+                System.out.println("removed");
+            }
+        }
 
     }
 
     @Override
     public Patient getPatientById(Long id) {
+        for (Patient p : dateBase.getPatients()) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
         return null;
     }
 
     @Override
     public Map<Integer, Patient> getPatientByAge() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Age?");
+        int age = scanner.nextInt();
+        for (Patient p : dateBase.getPatients()) {
+            if (p.getAge() == age) {
+                System.out.println(p);
+            }
+        }
+
         return null;
     }
 
     @Override
     public List<Patient> sortPatientsByAge(String ascOrDesc) {
+        if (ascOrDesc.equals("asc")) {
+            return dateBase.getPatients().stream().sorted(Comparator.comparing(Patient::getAge)).toList();
+        } else if (ascOrDesc.equals("desc")) {
+            return dateBase.getPatients().stream().sorted(Comparator.comparing(Patient::getAge).reversed()).toList();
+        }
         return null;
     }
 }
