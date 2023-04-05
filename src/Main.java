ff5d29e721fd8aa6a5098470e45026060b26a196
HP <adiibrahimov@gmail.com>
2023-04-05 21:01:02 +0600

    methods final

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ import service.impl.PatientServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-
+        PatientServiceImpl patientServiceImpl = new PatientServiceImpl();
+        Patient patient = new Patient(2L, "kjhdf", "lkjh", 33, Gender.MALE);
+        System.out.println(patientServiceImpl.addPatientToHospital(2L, patient));
+        DateBase dateBase = new DateBase();
+        System.out.println(dateBase.getHospitals());
+        System.out.println("---------------");
     }
 
 }
