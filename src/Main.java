b84f36741d84cb5509b5d50ae8c37175f72056f3
HP <adiibrahimov@gmail.com>
2023-04-05 20:51:04 +0600

    added sout s

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,7 @@ import service.impl.PatientServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        PatientServiceImpl patientServiceImpl = new PatientServiceImpl();
-        Patient patient = new Patient(2L, "kjhdf", "lkjh", 33, Gender.MALE);
-        System.out.println(patientServiceImpl.addPatientToHospital(2L, patient));
-        DateBase dateBase = new DateBase();
-        System.out.println(dateBase.getHospitals());
+
     }
 
 }
