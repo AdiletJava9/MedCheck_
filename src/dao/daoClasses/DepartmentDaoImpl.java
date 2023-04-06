753e1392fef22396f94d1922f60b8528d9382f05
Gulmira <gulmira.osmonova.ch@gmail.com>
2023-04-06 12:35:10 +0600

    second commit

--- src/dao/daoClasses/DepartmentDaoImpl.java
+++ src/dao/daoClasses/DepartmentDaoImpl.java
@@ -2,33 +2,117 @@ package dao.daoClasses;
 
 import dao.DepartmentDao;
 import db.DateBase;
+import exceptions.Ex;
 import model.Department;
+import model.Hospital;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DepartmentDaoImpl implements DepartmentDao {
     DateBase dateBase = new DateBase();
+    List<Hospital>hospitals=new ArrayList<>();
+
     @Override
     public String addDepartmentToHospital(Long id, Department department) {
-    return null;
-    }
-    @Override
-    public List<Department> getAllDepartmentByHospital(Long id) {
-        return null;
+        try{
+        boolean Loop = false;
+        for (Hospital hos : dateBase.getHospitals()) {
+            if (Objects.equals(hos.getId(), id)) {
+                hos.getDepartments().add(department);
+            } else {
+                Loop = true;
+            }
+        }
+            if (Loop) {
+                throw new Ex("Not found");
+            }
+        }catch (Ex e){
+            System.out.println(e.getMessage());
+        }
+        return "successfully added";
     }
 
     @Override
+    public List<Department> getAllDepartmentByHospital(Long id) {
+          try {
+              boolean b = true;
+              for (Hospital hospital:dateBase.getHospitals()) {
+                      if (hospital.getId().equals(id)){
+                          System.out.println(hospital.getDepartments());
+                      }else {
+                          b = false;
+                      }
+                  }
+              if (b){
+                  throw new Ex("Id not found");
+              }
+          }catch (Ex e){
+              System.out.println(e.getMessage());
+          }
+          return null;
+    }
+
+
+    @Override
     public Department findDepartmentByName(String name) {
+        try {
+            boolean loop = false;
+            for (Department department : dateBase.getDepartments()) {
+                if (department.getDepartmentName().equals(name)) {
+                    System.out.println(department);
+                }
+                else{
+                    loop = true;
+                }
+            }
+            if (loop){
+             throw new Ex("Name not found");
+            }
+        }catch (Ex e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public void deleteDepartmentById(Long id) {
-
+        try{
+            boolean b = false;
+            for (Department department : dateBase.getDepartments()) {
+                if (department.getId().equals(id)) {
+                    System.out.println(department);
+                }else {
+                    b=true;
+                }
+            }
+            if (b){
+                throw new Ex("Id not found");
+            }
+        }catch (Ex e){
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public String updateDepartmentById(Long id, Department department) {
+        try{
+            boolean b = true;
+            for (Department dept : dateBase.getDepartments()) {
+                if (dept.getId().equals(id)) {
+                    dept.setDepartmentName(department.getDepartmentName());
+                }else {
+                    b = false;
+                }
+            }
+            if (b){
+                throw new Ex("Id not found");
+            }
+                return "Department with id" + id + "update successfully";
+
+        }catch (Ex e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 }
